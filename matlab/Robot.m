c913197c34286bfd011a95398554e6fb5b3e23d0
Cheeseum <z.cheeseum@gmail.com>
2015-11-25 11:59:38 -0700

    generalized the orientation code for right and left

--- matlab/Robot.m
+++ matlab/Robot.m
@@ -42,7 +42,7 @@ classdef (Abstract) Robot < handle
         rightMotorForward(obj, powerPercent)
         rightMotorReverse(obj, powerPercent)
         allStop(obj)
-        getBatteryLife(obj)
+        getBatteryLevel(obj)
         rotateArm(obj, angleDegrees, powerPercent)
     end
 end
